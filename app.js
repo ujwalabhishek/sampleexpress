e434553b0cdb2e94dafc9eec164c57f3c896c35a
Ujwal Abhishek <ujwalabhishek@gmail.com>
2023-08-26 18:32:10 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/', (req, res) => {
+  res.send('Got a POST request')
+})
+
+app.put('/user', (req, res) => {
+  res.send('Got a PUT request at /user')
+})
+
+app.delete('/user', (req, res) => {
+  res.send('Got a DELETE request at /user')
+})
+
+app.get('/example/a', (req, res) => {
+  res.send('Hello from A!')
+})
+
+
+app.route('/book')
+  .get((req, res) => {
+    res.send('Get a random book')
+  })
+  .post((req, res) => {
+    res.send('Add a book')
+  })
+  .put((req, res) => {
+    res.send('Update the book')
+  })
+
+  
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
